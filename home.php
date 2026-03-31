47b92db6f0edc953efd603f00348c058ec336442
Joao-PedroDev07 <joaopedrodszss48@gmail.com>
2026-03-30 22:18:05 -0300

    carrosel

---
+++ home.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+
+    <?php include 'header1.php'; ?>
+
+    <div class="container-principal">
+
+        <div class="Carrosel">
+
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+
+                <div class="carousel-inner">
+
+                    <div class="carousel-item active">
+                        
+                        <img src="img/banner1.png" class="d-block w-100" alt="Banner 1">
+                    </div>
+
+                    <div class="carousel-item">
+                        
+                        <img src="img/banner2.png" class="d-block w-100" alt="Banner 2">
+                    </div>
+
+                    <div class="carousel-item">
+                
+                        <img src="img/banner3.png" class="d-block w-100" alt="Banner 3">
+                    </div>
+
+                </div>
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Anterior</span>
+                </button>
+
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Próximo</span>
+                </button>
+
+            </div>
+        </div>
+
+        <div class="Promoções">
+            
+        </div>
+
+        <div class="Produtos">
+
+        </div>
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
